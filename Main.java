c0f61e3aef523603a20a12650652d40101828420
vladFedurko <fedurko.v@gmail.com>
2019-10-21 11:54:28 +0300

    ADD: Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+//import java.util.Scanner;
+import Lab_1.MainLab1;
+import Lab_2.MainFrame;
+import Lab_3.Main3;
+
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+        //MainLab1.toStart(args);
+        MainFrame.toStart(args);
+        //Main3.toStart(args);
+    }
+}
